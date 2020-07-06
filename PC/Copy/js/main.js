3b2b5c0e8c499c1f518d66b2ac536f72a00f89b8
MineLone <18348270465@163.com>
2020-07-06 17:18:31 +0800

    22

---
+++ PC/Copy/js/main.js
@@ -0,0 +1,12 @@
+
+/*
+
+//适配移动端 768px 以下根目录
+(function() {
+    var htmlSize=document.documentElement.clientWidth ;
+    if (htmlSize < 768) {
+        document.documentElement.style.fontSize=htmlSize / 7.5 + 'px';
+    }
+})();
+
+*/
