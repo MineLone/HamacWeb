c9b3c1cc87d447ab8f8e7139363a839c65317bea
MineLone <18348270465@163.com>
2020-09-12 08:54:48 +0800

    111

---
+++ tuku/js/index.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    var mySwiper = new Swiper('#swiper-more', {
+      autoplay: 3000,//可选选项，自动滑动
+      speed:300,  //滑动需要的时间
+      slidesPerView :'auto',
+      loop: true, //循环
+      freeMode:true,  //贴齐
+      // grabCursor:true, //鼠标抓手
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
